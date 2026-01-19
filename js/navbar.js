0a6ce3e1d5c10488349c84253657cbd63ef417ba
VonRabbid <vonrabbid@gmail.com>
2026-01-18 23:47:41 -0500

    Correción 2.0

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-const menuToggle = document.getElementById('menu-toggle'); 
-    const menuIcon = document.querySelector('.menu-icon');    
-    const navMenu = document.querySelector('.menu');           
+    const menuToggle = document.getElementById('menu-toggle'); 
     const menuLinks = document.querySelectorAll('.menu a:not(.dropdown-toggle)');
     const dropdowns = document.querySelectorAll('.dropdown-beneficios, .dropdown-certificacion, .dropdown-comunidades');
-
-    if (menuIcon && menuToggle) {
-        menuIcon.addEventListener('click', function(e) {
-            e.preventDefault(); 
-            menuToggle.checked = !menuToggle.checked;
-        });
-    }
-
     menuLinks.forEach(link => {
         link.addEventListener('click', () => {
             if(menuToggle) menuToggle.checked = false;
